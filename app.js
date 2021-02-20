8e799695baf54a893e12b922ba5006d7968c1ae6
Nerzhin <glebvnerzhin@gmail.com>
2021-02-20 21:43:14 +0100

    can detect winner

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const WIDTH = 700;
 const HEIGHT = 600;
 const COLUMNS = 7;
 const ROWS = 6;
+const WINNING_SCORE = 4;
 const ANIMTATION_DURATION = 1000;
 
 const columnLength = WIDTH / COLUMNS;
 const rowLength = HEIGHT / ROWS;
+const rows = d3.range(1, ROWS + 1);
+const columns = d3.range(1, COLUMNS + 1);
 
-const grid = [
+const lineGrid = [
   { type: "row", data: d3.range(ROWS + 1) },
   { type: "column", data: d3.range(COLUMNS + 1) },
 ].reduce((a, d) => {
@@ -22,26 +25,34 @@ const xAxisColumns = (d) => d * columnLength;
 
 svg
   .selectAll("line")
-  .data(grid)
+  .data(lineGrid)
   .join("line")
-  .attr("x1", (d) => (d.type === "row" ? 0 : xAxisColumns(d.value)))
-  .attr("y1", (d) => (d.type === "row" ? yAxisRows(d.value) : 0))
-  .attr("x2", (d) => (d.type === "row" ? WIDTH : xAxisColumns(d.value)))
-  .attr("y2", (d) => (d.type === "row" ? yAxisRows(d.value) : HEIGHT));
+  .attr("x1", (gridLine) =>
+    gridLine.type === "row" ? 0 : xAxisColumns(gridLine.value)
+  )
+  .attr("y1", (gridLine) =>
+    gridLine.type === "row" ? yAxisRows(gridLine.value) : 0
+  )
+  .attr("x2", (gridLine) =>
+    gridLine.type === "row" ? WIDTH : xAxisColumns(gridLine.value)
+  )
+  .attr("y2", (gridLine) =>
+    gridLine.type === "row" ? yAxisRows(gridLine.value) : HEIGHT
+  );
 
 const getCenter = (target, length) => target * length - length / 2;
 
 const render = (selection, moves) => {
   selection
     .selectAll("circle")
-    .data(moves, (d) => d.target)
+    .data(moves, (move) => move.target)
     .join("circle")
-    .attr("class", (d) => d.player)
+    .attr("class", (move) => move.player)
     .attr("r", (2 / 3) * ((columnLength + rowLength) / 4))
-    .attr("cx", (d) => getCenter(d.target[1], columnLength))
+    .attr("cx", (move) => getCenter(move.target[1], columnLength))
     .transition()
-    .duration((d) => (d.target[0] / ROWS) * 1000)
-    .attr("cy", (d) => getCenter(d.target[0], rowLength));
+    .duration((move) => (move.target[0] / ROWS) * 1000)
+    .attr("cy", (move) => getCenter(move.target[0], rowLength));
 };
 
 let allowMove = true;
@@ -59,10 +70,80 @@ const moves = [];
 
 const move = (column) => {
   const player = moves.length % 2 ? "player-2" : "player-1";
-  const columnState = moves.map((d) => d.target).filter((d) => d[1] === column);
-  const row = columnState.length ? d3.min(columnState, (d) => d[0]) - 1 : ROWS;
+  const columnState = moves
+    .map((d) => d.target)
+    .filter((cell) => cell[1] === column);
+  const row = columnState.length
+    ? d3.min(columnState, (cell) => cell[0]) - 1
+    : ROWS;
   moves.push({ player: player, target: [row, column] });
   render(svg, moves);
+  console.log(checkForWinningLane());
+};
+
+const checkForWinningLane = () => {
+  const lanes = getLanes();
+  for (const lane of lanes) {
+    let matches = [];
+    for (const cell of lane) {
+      const occupier = getOccupier(cell);
+      if (!occupier) {
+        matches = [];
+      } else if (!matches.length) {
+        matches.push({ player: occupier, cell: cell });
+      } else if (matches[matches.length - 1].player === occupier) {
+        matches.push({ player: occupier, cell: cell });
+      } else {
+        matches = [];
+      }
+      if (matches.length === WINNING_SCORE)
+        return matches.map((match) => match.cell);
+    }
+  }
+  return false;
+};
+
+const getOccupier = (cell) => {
+  const move = moves.find(
+    (move) => move.target[0] === cell[0] && move.target[1] === cell[1]
+  );
+  if (move) return move.player;
+  return undefined;
+};
+
+const getLanes = () => {
+  const rowLanes = rows.map((x) => columns.map((y) => [x, y]));
+  const columnLanes = columns.map((y) => rows.map((x) => [x, y]));
+  const diagonalLanes = [
+    ...rows.map((x) => [x, 1]),
+    ...rows.map((x) => [x, COLUMNS]),
+    ...columns.map((y) => [1, y]),
+  ].reduce((lanes, cell) => {
+    const newLanes = [];
+    [
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ].forEach((steps) => {
+      const lane = [];
+      let nextCell = [...cell];
+      while (
+        nextCell[0] > 0 &&
+        nextCell[0] <= ROWS &&
+        nextCell[1] > 0 &&
+        nextCell[1] <= COLUMNS
+      ) {
+        lane.push(nextCell);
+        nextCell = nextCell.map((d, i) => d + steps[i]);
+      }
+      newLanes.push(lane);
+    });
+    return [...lanes, ...newLanes];
+  }, []);
+  return [...rowLanes, ...columnLanes, ...diagonalLanes].filter(
+    (lanes) => lanes.length >= WINNING_SCORE
+  );
 };
 
 document.addEventListener("keydown", handleKeydown);
